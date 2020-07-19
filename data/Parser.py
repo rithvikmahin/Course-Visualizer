59cce2871db59cd013f60a7bc323e3e531debf60
Rithvik <rithvik7@illinois.edu>
2020-07-18 23:33:55 -0400

    feat(Parsing): Added prerequisites to JSON
    Co-authored-by: Samraj Moorjani <samrajmoorjani@gmail.com>

--- data/Parser.py
+++ data/Parser.py
@@ -19,21 +19,34 @@ course_list = list(map(lambda x: x.replace('\xa0', ''), course_list))
 courses_dict = {}
 
 for course in course_list[1:]:
+    # Removes empty and newline characters
     course = list(filter(lambda x: x != '', course.split("\n")))
     course_details = course[0].split("\u2002")
-    print(course)
     course_info = course[1].split('Prerequisite:')
 
     course_dict = {
         'number': course_details[0].strip(),
         'name': course_details[1].strip(),
-        'credits': course_details[2].replace('credit:', '').replace('Hour.', '').strip(),
+        'credits': course_details[2].replace('credit:', '').replace('Hour.', '').replace('Hours.', '').strip(),
         'description': course_info[0].strip(),
     }
 
+    # Checks if a prerequisite exists
+    prereq_dict = {}
     if len(course_info) > 1:
-        course_dict['prerequisite'] = course_info[1].strip()
+        prerequisite = course_info[1].strip().upper().replace(": ", "").replace(" OR", ",").replace(" AND", ";").replace('ONE OF', '').strip().split(".")[0]
+        # Splitting AND values based on semicolons and OR values after that based on commas
+        # Also removes empty values
+        split_values = [list(filter(lambda x: x != '', map(lambda x: x.strip(), string.split(","))))
+                        for string in prerequisite.split(";")]
 
+        #Adding each requisite to the JSON dictionary
+        for i, value in enumerate(split_values):
+            prereq_dict['req' + str(i + 1)] = value
+        print(split_values)
+        
+        
+    course_dict['prerequisite'] = prereq_dict
     courses_dict[course_dict['number'].strip()] = course_dict
     
 with open('Courses.json', 'w') as json_file:
