a4318cc3d1db71b0deaf5cd58a995a8a9c0cd47e
Rithvik <rithvik7@illinois.edu>
2020-07-22 17:02:36 -0400

    Enabled Cytoscape

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import cytoscape from 'cytoscape'
+
+
 
 function App() {
+
+  useEffect(() => {
+    const Cytoscape = () => {
+      const graph = cytoscape();
+      graph.add({group: 'nodes',
+      data: { weight: 75 },
+      position: { x: 200, y: 200 }
+      })
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
