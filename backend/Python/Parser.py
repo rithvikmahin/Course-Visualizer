9ce895e6198eb36c8db04f0df15c16c471e0f377
Rithvik <rithvik7@illinois.edu>
2020-07-28 16:21:11 -0400

    Populated Neo4j database with the courses JSON data

--- backend/Python/Parser.py
+++ backend/Python/Parser.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 
 # Replacement mapping for credits strings
 CREDITS_REPLACE_MAPPING = {
-    'credit:', '',
-    'Hour.', '',
-    'Hours.', '',
+    'credit:': '',
+    'Hour.': '',
+    'Hours.': ''
 }
 
 # Replacement mapping for prereq strings
@@ -29,7 +29,7 @@ def replace_with_dict(text: str, mapping: dict):
     return expr.sub(lambda x: mapping[x.string[x.start():x.end()]], text)
 
 def catalog_to_json(catalog_url: str = 'http://catalog.illinois.edu/courses-of-instruction/cs/',
-                    output_dir: str = '../../src/json'):
+                    output_dir: str = './neo4j/json/Courses.json'):
     """Takes URL to course catalog and returns JSON with all necessary data.
 
     :param catalog_url: url of catalog data to retrieve
@@ -67,14 +67,21 @@ def get_course_dict(courses: list):
         course_dict = {
             'number': course_details[0].replace(' ',''),
             'name': course_details[1],
-            'credits': course_details[2].replace_with_dict(CREDITS_REPLACE_MAPPING)
+            'credits': '',
             'description': course_info[0],
         }
+
+        for key, value in CREDITS_REPLACE_MAPPING.items():
+            course_dict['credits'] = course_details[2].replace(key, value)
 
         # Checks if a prerequisite exists
         prereq_dict = {}
         if len(course_info) > 1:
-            prerequisite = course_info[1].strip().upper().replace_with_dict(PREREQ_REPLACE_MAPPING).split(".")[0]
+            prerequisite = course_info[1].strip().upper()
+
+            for key, value in PREREQ_REPLACE_MAPPING.items():
+                prerequisite = prerequisite.replace(key, value).split(".")[0]
+
             # Splitting AND values based on semicolons and OR values after that based on commas
             # Also removes empty values
             split_values = [list(filter(lambda x: x != '', map(lambda x: x.strip(),
--- backend/neo4j/database.ts
+++ backend/neo4j/database.ts
@@ -1,8 +1,9 @@
 require('dotenv').config();
+const courses =  require('./json/Courses.json');
 const neo4j = require('neo4j-driver');
 
-async function Create() {
-    /** TODO: Replace username and password with environment variables */
+async function Populate() {
+
     const driver = neo4j.driver(
         'bolt://localhost',
          neo4j.auth.basic(`${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`)
@@ -11,7 +12,37 @@ async function Create() {
     
     try {
         await session.writeTransaction(async txc => {
-            const result = await txc.run(`CREATE (n: name {name: 'Alice'}) RETURN n`);
+            for (const course in courses) {
+                const subject = course.replace(/[0-9]/g, '');
+                const courseNumber = course.toLowerCase().replace(/['a-z']/g, '');
+                const name = courses[course]['name'];
+                const credits = courses[course]['credits'];
+                const description = courses[course]['description'];
+                const node = await txc.run(`MATCH (node: ${subject}) WHERE node.number = ${courseNumber} RETURN node`);
+
+                if (node.records.length == 0) {
+                    /** TODO: Description only works if it is in double quotes */
+                    const create = await txc.run(`CREATE (node: CS {number: ${courseNumber}, name: '${name}', 
+                    credits: '${credits}', description: "${description}"}) RETURN node `);
+                }
+
+                for (const requirements in courses[course]['prerequisite']) {
+                    for (const required_course of courses[course]['prerequisite'][requirements]) {
+                        /** Removes junk course names that are not in the standard format. */
+                        /** TODO:  Remove Concurrent Registration or save it in another format. remove length > 8 */
+                        const regex_filter = new RegExp('[A-Z]{2,5}\\d{2,3}');
+                        const maximumCourseLength = 8;
+
+                        if (!(regex_filter.test(required_course)) || required_course.length > maximumCourseLength) {
+                            continue;
+                        }
+                        const prerequisiteNumber = required_course.toLowerCase().replace(/['a-z']/g, '');
+                        const connection = await txc.run(`MATCH (prerequisite : ${subject}), (course : ${subject}) 
+                        WHERE prerequisite.number = ${prerequisiteNumber} AND course.number = ${courseNumber}
+                        CREATE (prerequisite)-[edge : FOR]->(course) RETURN type(edge)`);
+                    }
+                }
+            }   
         });
     } finally {
         console.log('Finished!');
@@ -20,5 +51,5 @@ async function Create() {
     }
 }
 
-Create();
+Populate();
 
