03d24fc94e644f194d38965c3fc8321f9ed3fa07
Rithvik <rithvik7@illinois.edu>
2020-08-04 15:33:48 -0400

    Integrated fuse into the front end to autosearch.

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../css/style.css'
 import Fuse from 'fuse.js'
 
@@ -7,6 +7,11 @@ function Search(data: any) {
     const [searchTerm, setSearchTerm] = useState("");
     const [searchBar, setSearchBar] = useState(null);
     const [courses, setCourses] = useState(null);
+    const [fuseList, setFuseList] = useState(null);
+    const stateRef = useRef();
+
+    //@ts-ignore
+    stateRef.current = fuseList ? fuseList[0]['item']['subject'] + fuseList[0]['item']['number'] : '';
 
     let fuse: string;
     const options = {
@@ -14,13 +19,15 @@ function Search(data: any) {
     }
     
     const handleSearch = () => {
+
       if (searchBar) {
         //@ts-ignore
         setSearchTerm(searchBar.value);
       }
       if (searchTerm) {
-        const result = fuse.search(searchTerm);
-        console.log(result);
+        const result: any = fuse.search(searchTerm);
+        //@ts-ignore
+        setFuseList(result);
       }
     }
 
@@ -41,6 +48,9 @@ function Search(data: any) {
     return (
         <div>
             <input type='text' id='search-bar' placeholder='search' onChange={handleSearch} value={searchTerm}/>
+            {/**
+             //@ts-ignore */}
+            <div style={{color: 'white'}}>{stateRef.current}</div>
         </div>
     )
 }
