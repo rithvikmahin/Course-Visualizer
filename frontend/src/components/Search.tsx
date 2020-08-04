e1c47ffda565bf61752925f873a77c452240184c
Rithvik <rithvik7@illinois.edu>
2020-08-04 16:02:36 -0400

    Updated the case where invalid characters were entered.

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -4,6 +4,11 @@ import Fuse from 'fuse.js'
 import Courses from '../types/json'
 import Data from '../types/data'
 
+/**
+ * 
+ * @param data - The object containing the course data, called from the internal API.
+ */
+
 function Search(data: Data) {
 
     const [searchTerm, setSearchTerm] = useState("");
@@ -11,21 +16,26 @@ function Search(data: Data) {
     const [courses, setCourses] = useState(null);
     const [fuseList, setFuseList] = useState(null);
     const stateRef = useRef();
-
-    stateRef.current = fuseList && searchTerm ? fuseList[0]['item']['subject'] + fuseList[0]['item']['number'] : '';
+    
+    // Sets the state reference to display the list of search results.
+    stateRef.current = fuseList && fuseList.length > 0 && 
+                       searchTerm ? fuseList[0]['item']['subject'] + fuseList[0]['item']['number'] : '';
 
     let fuse: {[key: string]: any};
+
+    // The fuse.js options.
     const options = {
       keys: ['subject', 'number']
     }
     
     const handleSearch = () => {
-
       if (searchBar) {
         setSearchTerm(searchBar.value);
       }
       if (searchTerm) {
-        const result: Courses = fuse.search(searchTerm);
+        // Removes whitespace from the search.
+        const query = searchTerm.replace(/\s/g, '');
+        const result: Courses = fuse.search(query);
         setFuseList(result);
       }
     }
